1a3ad646ade3633242a73066af24aca7709cb185
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-22 15:26:09 -0800

    Functional server.py

---
+++ server.py
@@ -0,0 +1,128 @@
+"""Greeting Flask app."""
+
+from random import choice
+
+from flask import Flask, request
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
+    'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
+    'wonderful', 'smashing', 'lovely']
+
+Disses = [
+    "hideous", "mugly", "a hot ass mess", "a turd", "an accident"]
+
+@app.route("/")
+def start_here():
+    """Home page."""
+
+    return """<!doctype html>
+                <html>Hi! This is the home page.</html>
+                <a href="/hello">Hello page</a>
+                """
+
+
+@app.route("/hello")
+def say_hello():
+    """Say hello and prompt for user's name."""
+    s = ""
+    d = ""
+    for compliment in AWESOMENESS:
+      # s = s+f'<option value="{compliment}">{compliment}</option>'
+      s = s+'<option value="{}">{}</option>'.format(compliment,compliment)
+
+    for diss in Disses:
+      d = d+'<option value="{}">{}</option>'.format(diss,diss)
+
+    return f"""
+    <!doctype html>
+    <html>
+      <head>
+        <title>Hi There!</title>
+      </head>
+      <body>
+        <h1>Hi There!</h1>
+        <form action="/greet">
+          What's your name? <input type="text" name="person">
+          <br>
+          <br>
+          Compliment:
+            <select name = "compliment">
+              {s}
+              </select>
+            <br>
+            <br>
+            <input type="submit" value="Submit">
+        </form>
+        <br>
+        <form action="/diss">
+          What's your name? <input type="text" name="person">
+          <br>
+          <br>
+          Diss:
+            <select name = "diss">
+              {d}
+              </select>
+            <br>
+            <br>
+            <input type="submit" value="Submit">
+        </form>
+      </body>
+    </html>
+    """
+
+
+@app.route("/greet")
+def greet_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+    compliment = request.args.get("compliment")
+    #compliment = choice(AWESOMENESS)
+
+    return f"""
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi, {player}! I think you're {compliment}!
+      </body>
+    </html>
+    """
+
+
+@app.route("/diss")
+def diss_person():
+  """Diss person harshly by name."""
+
+
+  player = request.args.get("person")
+  diss = request.args.get("diss")
+    
+
+  return """
+  <!doctype html>
+  <html>
+    <head>
+      <title>A Diss</title>
+    </head>
+    <body>
+      Hi, {}! I think you're {}!
+    </body>
+  </html>
+  """.format(player, diss)
+
+
+
+
+if __name__ == "__main__":
+    # debug=True gives us error messages in the browser and also "reloads"
+    # our web app if we change the code.
+    app.run(debug=True, host="0.0.0.0")
+
